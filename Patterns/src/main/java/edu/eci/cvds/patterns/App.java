1fa0a43b96be7ea5abce764c41d8339ad434104c
unknown <edgar.henao@labinfo.is.escuelaing.edu.co>
2022-01-29 08:58:35 -0500

    1

--- Patterns/src/main/java/edu/eci/cvds/patterns/App.java
+++ Patterns/src/main/java/edu/eci/cvds/patterns/App.java
@@ -8,6 +8,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+		if (args.length == 0) System.out.println("No ingreso un carajo");
+		String cadena = "Hello";
+		for (int i = 0; i < args.length; i ++){
+		    cadena += " ";
+            cadena += args[i];
+        }
+		cadena += "!";
+		System.out.println(cadena);
     }
 }
